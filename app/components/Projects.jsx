8f38b97b3177a2159c25d2b4f1f738242d8fd498
mohimamrifai <imamrifaibisnis@gmail.com>
2024-09-29 00:45:56 +0700

    add card project

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,6 +1,52 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+
+const projects = [
+  {
+    id: 1,
+    title: "Digify | Digital Marketing Landing Page",
+    href: "https://digify-pi.vercel.app/",
+    src: "/project1.png",
+  },
+  {
+    id: 2,
+    title: "Umahku | Furniture Shop",
+    href: "#",
+    src: "/project2.png",
+  },
+  {
+    id: 3,
+    title: "Website Seblak Prasmanan",
+    href: "https://seblak-prasmanan-lubis.vercel.app/",
+    src: "/project3.png",
+  },
+  {
+    id: 4,
+    title: "Website Digital Bank",
+    href: "#",
+    src: "/project4.png",
+  },
+  {
+    id: 5,
+    title: "Mirland | Jasa Website",
+    href: "https://mirland-id.vercel.app",
+    src: "/project5.png",
+  },
+  {
+    id: 6,
+    title: "Easy Rent | Rental Mobile",
+    href: "https://rent-mir.vercel.app/",
+    src: "/project6.png",
+  },
+  {
+    id: 7,
+    title: "Website Bakso Sapi Mang Budi",
+    href: "https://baksomangbudi.vercel.app/",
+    src: "/project7.png",
+  },
+];
 
 export default function Projects() {
   return (
@@ -19,15 +65,18 @@ export default function Projects() {
           This is a project I have worked on.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 mt-20 gap-5 relative">
-          {Array.from({ length: 7 }).map((_, index) => (
-            <Image
-              key={index}
-              src={`/project${index + 1}.png`}
-              alt="project 1"
-              width={1000}
-              height={1000}
-              className="rounded shadow-md"
-            />
+          {projects.map((item) => (
+            <div key={item.id} className="bg-white rounded overflow-hidden">
+              <Image
+                src={item.src}
+                alt="project 1"
+                width={1000}
+                height={1000}
+              />
+              <div className="p-4">
+                <Link href={item.href} target="_blank" className="font-semibold text-xl">{item.title}</Link>
+              </div>
+            </div>
           ))}
           <div className="absolute w-full h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 blur-3xl bottom-0 left-0 -z-20 opacity-60"></div>
         </div>
